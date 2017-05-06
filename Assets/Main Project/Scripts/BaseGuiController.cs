a77986278164ef3d46beb6011a9307abc9ec07a9
Xiao-Ping Huynh <Xiao-Ping Huynh>
2017-05-05 20:31:20 -0400

    some UI work

--- Assets/Main Project/Scripts/BaseGuiController.cs
+++ Assets/Main Project/Scripts/BaseGuiController.cs
@@ -3,75 +3,76 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class BaseGuiController : MonoBehaviour {
-    public CellGrid CellGrid;
-    public GameObject UnitsParent;
-    public Button NextTurnButton;
+  public CellGrid CellGrid;
+  public GameObject UnitsParent;
+  public Button NextTurnButton;
 
-    public Image UnitImage;
-    public Text InfoText;
-    public Text StatsText;
+  //public Image UnitImage;
+  public Text InfoText;
+  public Text StatsText;
+  private GameObject StatsPanel;
+  private GameObject AttackPanel;
 
-    void Start() {
-        UnitImage.color = Color.gray;
+  void Start() {
+    //UnitImage.color = Color.gray;
+    StatsPanel = GameObject.Find("Stats Panel");
+    CellGrid.GameStarted += OnGameStarted;
+    CellGrid.TurnEnded += OnTurnEnded;
+    CellGrid.GameEnded += OnGameEnded;
+  }
 
-        CellGrid.GameStarted += OnGameStarted;
-        CellGrid.TurnEnded += OnTurnEnded;
-        CellGrid.GameEnded += OnGameEnded;
+  private void OnGameStarted(object sender, EventArgs e) {
+    foreach (Transform unit in UnitsParent.transform) {
+      unit.GetComponent<Unit>().UnitHighlighted += OnUnitHighlighted;
+      unit.GetComponent<Unit>().UnitDehighlighted += OnUnitDehighlighted;
+      unit.GetComponent<Unit>().UnitAttacked += OnUnitAttacked;
     }
-
-    private void OnGameStarted(object sender, EventArgs e) {
-        foreach (Transform unit in UnitsParent.transform) {
-            unit.GetComponent<Unit>().UnitHighlighted += OnUnitHighlighted;
-            unit.GetComponent<Unit>().UnitDehighlighted += OnUnitDehighlighted;
-            unit.GetComponent<Unit>().UnitAttacked += OnUnitAttacked;
-        }
-
-        foreach (Transform cell in CellGrid.transform) {
-            cell.GetComponent<Cell>().CellHighlighted += OnCellHighlighted;
-            cell.GetComponent<Cell>().CellDehighlighted += OnCellDehighlighted;
-        }
-
-        OnTurnEnded(sender, e);
+    foreach (Transform cell in CellGrid.transform) {
+      cell.GetComponent<Cell>().CellHighlighted += OnCellHighlighted;
+      cell.GetComponent<Cell>().CellDehighlighted += OnCellDehighlighted;
     }
+    OnTurnEnded(sender, e);
+  }
 
-    private void OnGameEnded(object sender, EventArgs e) {
-        InfoText.text = "Player " + ((sender as CellGrid).CurrentPlayerNumber + 1) + " wins!";
-    }
-    private void OnTurnEnded(object sender, EventArgs e) {
-        NextTurnButton.interactable = ((sender as CellGrid).CurrentPlayer is HumanPlayer);
-
-        InfoText.text = "Player " + ((sender as CellGrid).CurrentPlayerNumber + 1);
-    }
-    private void OnCellDehighlighted(object sender, EventArgs e) {
-        UnitImage.color = Color.gray;
-        StatsText.text = "";
-    }
-    private void OnCellHighlighted(object sender, EventArgs e) {
-        UnitImage.color = Color.gray;
-        StatsText.text = "Movement Cost: " + (sender as Cell).MovementCost;
-    }
-    private void OnUnitAttacked(object sender, AttackEventArgs e) {
-        if (!(CellGrid.CurrentPlayer is HumanPlayer)) return;
-        OnUnitDehighlighted(sender, new EventArgs());
-
-        if ((sender as Unit).HitPoints <= 0) return;
-
-        OnUnitHighlighted(sender, e);
-    }
-    private void OnUnitDehighlighted(object sender, EventArgs e) {
-        StatsText.text = "";
-        UnitImage.color = Color.gray;
-    }
-    private void OnUnitHighlighted(object sender, EventArgs e) {
-        var unit = sender as FEUnit;
+  private void OnGameEnded(object sender, EventArgs e) {
+    InfoText.text = "Player " + ((sender as CellGrid).CurrentPlayerNumber + 1) + " wins!";
+  }
+  private void OnTurnEnded(object sender, EventArgs e) {
+    NextTurnButton.interactable = ((sender as CellGrid).CurrentPlayer is HumanPlayer);
+    InfoText.text = "Player " + ((sender as CellGrid).CurrentPlayerNumber + 1);
+  }
+  private void OnCellDehighlighted(object sender, EventArgs e) {
+    //UnitImage.color = Color.gray;
+    StatsPanel.SetActive(false);
+    StatsText.text = "";
+  }
+  private void OnCellHighlighted(object sender, EventArgs e) {
+    //UnitImage.color = Color.gray;
+    StatsPanel.SetActive(true);
+    StatsText.text = (sender as HexCell).Terrain + 
+    "\nMovement Cost: " + (sender as Cell).MovementCost;
+  }
+  private void OnUnitAttacked(object sender, AttackEventArgs e) {
+    if (!(CellGrid.CurrentPlayer is HumanPlayer)) return;
+    OnUnitDehighlighted(sender, new EventArgs());
+    if ((sender as Unit).HitPoints <= 0) return;
+    OnUnitHighlighted(sender, e);
+  }
+  private void OnUnitDehighlighted(object sender, EventArgs e) {
+    StatsPanel.SetActive(false);
+    StatsText.text = "";
+    //UnitImage.color = Color.gray;
+  }
+  private void OnUnitHighlighted(object sender, EventArgs e) {
+    StatsPanel.SetActive(true);
+    var unit = sender as FEUnit;
     StatsText.text = unit.UnitName + "\nHit Points: " + unit.HitPoints + "/" + unit.TotalHitPoints + "\nMelee Attack: "
     + unit.MeleeAttack + "\nRanged Attack: " + unit.RangedAttack + "\nMagic Attack: " + unit.MagicAttack +
     "\nRange: " + unit.RangedRange + "\nMagic Range: " + unit.MagicRange + "\nAmmunition: " + unit.Ammunition +
     "\nMelee Defense: " + unit.MeleeDefense + "\nArmor: " + unit.Armor + "\nResistance: " + unit.Resistance +
     "\nMorale: " + unit.Morale;
         //UnitImage.color = unit.PlayerColor;
-
-    }
+  }
 
     public void RestartLevel() {
         Application.LoadLevel(Application.loadedLevel);
